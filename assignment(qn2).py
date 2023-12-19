68067ef944053db5c29799375320ddff5ea5c9c3
anjusaji2001 <145313179+anjusaji2001@users.noreply.github.com>
2023-12-19 11:40:43 +0530

    Add files via upload

---
+++ assignment(qn2).py
@@ -0,0 +1,11 @@
+a = 33
+b = 44
+c = 22
+largest = 0
+if a > b and a > c :
+    largest = a
+elif b > c :
+    largest = b
+else :
+    largest = c
+print(largest, "is the largest of three numbers.")
